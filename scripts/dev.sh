c0e5474ff5b8f285a4ab4e88dd1c280a4887748f
znotfireman <znotfireman@gmail.com>
2026-06-09 22:19:11 +0700

    make ur packages better oh my god

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -11,4 +11,4 @@ fi
 rojo sourcemap sourcemap.project.json -o sourcemap.json --watch \
     & darklua process --config dev.darklua.json --watch src/ dist/src \
     & darklua process --config dev.darklua.json --watch scripts/run-tests.server.luau dist/run-tests.server.luau \
-    & rojo serve dev.project.json
+    # & rojo serve dev.project.json
--- scripts/release.sh
+++ scripts/release.sh
@@ -8,4 +8,4 @@ darklua process --config default.darklua.json src/ dist/src
 rm -rf dist/src/__tests__
 rm -rf dist/src/jest.config.luau
 rojo build default.project.json -o CullThrottle.rbxm
-wally publish
+# wally publish
